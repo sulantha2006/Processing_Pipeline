71aa80a4639f5eefb5c121890cfd92eb40bd8017
Seqian Wang <seqian@outlook.com>
2015-08-23 08:16:48 -0400

    fMRI Conversion + NIAK Template
    Introducing a ConverterConfig file for minc commands location, Worked on
    the AdniTofMRI Pipeline, improved niakTemplate replacing string function

--- Config/ConverterConfig.py
+++ Config/ConverterConfig.py
@@ -4,3 +4,8 @@ studyTypeForConvertionDict = {'ADNI':{'AV45': 'AV45',
                                       'FDG': 'FDG',
                                       'ext-rsfmri': 'rsfmri',
                                       'rsfmri': 'rsfmri'}}
+
+dcmToMnc_exec = '/home/vfonov/quarantine/bin/dcm2mnc32'
+dcmToNii_exec = ''
+niiToMnc_exec = ''
+mincSource_exec = 'source /opt/minc-toolkit/minc-toolkit-config.sh'
--- Converters/ConversionScripts/ADNI_V1_FMRI.py
+++ Converters/ConversionScripts/ADNI_V1_FMRI.py
@@ -1,18 +1,148 @@
-__author__ = 'sulantha'
+__author__ = 'wang'
+import subprocess
+import os
+import fnmatch
+import distutils.dir_util
+import distutils.file_util
+import shutil
+import glob
+from Utils.PipelineLogger import PipelineLogger
+import Config.ConverterConfig
+
 
 class ADNI_V1_FMRI:
     def __init__(self):
         pass
 
     def convert_nii(self, convertionObj):
-        pass
+        rawFile = '{0}/*.nii*'.format(convertionObj.raw_folder)
+        outFile = '{0}/{1}_{2}{3}{4}{5}_{6}.mnc'.format(convertionObj.converted_folder, convertionObj.study,
+                                                        convertionObj.rid, convertionObj.scan_date.replace('-', ''),
+                                                        convertionObj.s_identifier, convertionObj.i_identifier,
+                                                        convertionObj.scan_type)
+		# Run nii2mnc
+		cmd = '{0} {1} {2}/../'.format('mv ' + rawFile + ' outFile')
+        PipelineLogger.log('converter', 'info',
+                           'nii2mnc conversion starting for : {0} - {1} - {2} - {3}'.format(convertionObj.study,
+                                                                                         convertionObj.rid,
+                                                                                         convertionObj.scan_date,
+                                                                                         convertionObj.scan_type))
+        PipelineLogger.log('converter', 'debug', 'Command : {0}'.format(cmd))
 
+		cmd = '%s %s %s' % (Config.ConverterConfig.niiToMnc_exec, rawFile, outFile)
+		self.runShellCommand(cmd)
+		self.checkMncFile(outFile) # Check whether the fMRI file has a time component/axis
+		
     def convert_v(self, convertionObj):
         pass
 
     def convert_dicom(self, convertionObj):
-        pass
+        rawFolder = convertionObj.raw_folder
+        outFile = '{0}/{1}_{2}{3}{4}{5}_{6}.mnc'.format(convertionObj.converted_folder, convertionObj.study,
+                                                        convertionObj.rid, convertionObj.scan_date.replace('-', ''),
+                                                        convertionObj.s_identifier, convertionObj.i_identifier,
+                                                        convertionObj.scan_type)
+		
+		tempFolder = converterObj.converted_folder + '/../temp'
+		self.createNewFolder(tempFolder)
+		# Move all the non-dicom stuff out of the original folder into tempFolder
+		otherFiles = self.removeOtherFilesInFolder(rawFolder, ".dcm", tempFolder)
+		# Run dcm2nii
+		dcm2nii_command = Config.ConverterConfig.dcmToNii_exec + ' -a N -e N -p N -o ' + tempFolder + '/ -v Y ' + rawFolder
+        cmd = '{0} {1} {2}/../'.format(dcm2nii_command, rawFolder, convertionObj.converted_folder)		
+        PipelineLogger.log('converter', 'info',
+                           'dcm2nii conversion starting for : {0} - {1} - {2} - {3}'.format(convertionObj.study,
+                                                                                         convertionObj.rid,
+                                                                                         convertionObj.scan_date,
+                                                                                         convertionObj.scan_type))
+        PipelineLogger.log('converter', 'debug', 'Command : {0}'.format(cmd))
+		# Move all the non-dicom stuff back into the original folder
+		self.addBackOtherFiles(rawFolder,otherFiles,tempFolder)
+		# Run nii2mnc
+		self.createNewFolder(convertionObj.converted_folder)
+        cmd = '{0} {1} {2}/../'.format(Config.ConverterConfig.niiToMnc_exec, rawFolder, convertionObj.converted_folder)		
+        PipelineLogger.log('converter', 'info',
+                           'nii2mnc conversion starting for : {0} - {1} - {2} - {3}'.format(convertionObj.study,
+                                                                                         convertionObj.rid,
+                                                                                         convertionObj.scan_date,
+                                                                                         convertionObj.scan_type))
+        PipelineLogger.log('converter', 'debug', 'Command : {0}'.format(cmd))
+		iterator = 1
+		for niiFile in glob.glob(tempFolder + '/*.nii.gz'):
+			tempOutFile = outFile.replace('.mnc', '_' + iterator + '.mnc')
+			cmd = '%s %s %s' % (Config.ConverterConfig.niiToMnc_exec, niiFile, tempOutFile)
+			self.runShellCommand(cmd)
+			self.checkMncFile(tempOutFile) # Check whether the fMRI files have a time component/axis
+			iterator = iterator + 1
+		# Delete Temporary Folder
+		self.deleteFolder(tempFolder)
+		
+		# Check how many mnc files were generated
+        mncList = []
+        for root, dirnames, filenames in os.walk('{0}/../'.format(convertionObj.converted_folder)):
+            for filename in fnmatch.filter(filenames, '*.mnc'):
+                mncList.append(os.path.join(root, filename))
+        if len(mncList) == 0:
+            PipelineLogger.log('converter', 'error',
+                               'MINC Conversion unsuccessful : Check log for : {0} - {1} - {2} - {3}'.format(
+                                   convertionObj.study, convertionObj.rid, convertionObj.scan_date,
+                                   convertionObj.scan_type))
+            return 0
+		else
+            return 1
 
     def convertMinc(self, convertionObj):
-        pass
+        rawFile = '{0}/*.mnc'.format(convertionObj.raw_folder)
+        outFile = '{0}/{1}_{2}{3}{4}{5}_{6}.mnc'.format(convertionObj.converted_folder, convertionObj.study,
+                                                        convertionObj.rid, convertionObj.scan_date.replace('-', ''),
+                                                        convertionObj.s_identifier, convertionObj.i_identifier,
+                                                        convertionObj.scan_type)
+		# Move files
+		cmd = '{0} {1} {2}/../'.format('mv ' + rawFile + ' outFile')
+        PipelineLogger.log('converter', 'info',
+                           'MINC transfer starting for : {0} - {1} - {2} - {3}'.format(convertionObj.study,
+                                                                                         convertionObj.rid,
+                                                                                         convertionObj.scan_date,
+                                                                                         convertionObj.scan_type))
+        PipelineLogger.log('converter', 'debug', 'Command : {0}'.format(cmd))
+		self.runShellCommand(cmd)
+														
+	def createNewFolder(self, folder):
+		self.deleteFolder(folder)
+        try:
+            distutils.dir_util.mkpath(folder)
+        except:
+            pass
+			
+	def deleteFolder(self, folder):
+		try:
+            shutil.rmtree('{0}/../'.format(folder))
+        except:
+            pass
 
+	def checkMncFile(self, mncFile):
+		info_command = Config.ConverterConfig.mincSource_exec + '; mincinfo ' + mncFile + ' | grep \"time\" '
+	    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = p.communicate()
+		if not out: # If no output, string empty
+			PipelineLogger.log('converter', 'debug', mncFile + 'does not have a time axis!')
+			os.remove(mncFile)			
+
+	def runShellCommand(self, cmd):
+	    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        PipelineLogger.log('converter', 'debug', 'Conversion Log Output : \n{0}'.format(out))
+        PipelineLogger.log('converter', 'debug', 'Conversion Log Err : \n{0}'.format(err))
+		
+	def removeOtherFilesInFolder(t_folderPath, extToKeep,temp_file_folder):
+    otherFiles = []
+    for file in os.listdir(t_folderPath):
+        if not file.endswith(extToKeep):
+            shutil.move(t_folderPath + "/" + file, temp_file_folder)
+            otherFiles.append(file)
+    return otherFiles
+	
+	def addBackOtherFiles(folderPath,otherFiles,temp_file_folder):
+    for file in otherFiles:
+        shutil.move(temp_file_folder + '/' + file, folderPath + "/")
+		
