09013ac9a9ef906698e92d5dcf984554a8c6a4a7
sulantha <sulantha.s@gmail.com>
2015-08-25 13:35:32 -0400

    Added FMRI Versions

--- Config/StudyConfig.py
+++ Config/StudyConfig.py
@@ -8,7 +8,8 @@ AllowedModalityList = ['T1', 'AV45', 'FDG', 'MT1__GradWarp__N3m', 'MT1__N3m']
 #This dictionary will have the list of modalities for each study type, where only these type of images be moved and processed.
 ProcessingImagingModalities = {'ADNI':('T1', 'AV45', 'FDG', 'MT1_G_N3m', 'MT1_N3m','rsfmri','ext-rsfmri')}
 
-defaultVersioningForStudy = {'ADNI':{'T1':'V1', 'AV45':'V1', 'FDG':'V1', 'MT1_G_N3m':'V1', 'MT1_N3m':'V1'}}
+defaultVersioningForStudy = {'ADNI':{'T1':'V1', 'AV45':'V1', 'FDG':'V1', 'MT1_G_N3m':'V1', 'MT1_N3m':'V1',
+                                     'rsfmri':'V1','ext-rsfmri':'V1'}}
 
 ADNIDownloadRoot = '/data/backup-data02/ADNI/downloads/New2'
 
