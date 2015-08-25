2118ccb10dd1880151b0332c98520db01702726b
sulantha <sulantha.s@gmail.com>
2015-08-25 16:26:43 -0400

    ADDING LIBS

---
+++ Config/LIB_PATH.py
@@ -0,0 +1,13 @@
+__author__ = 'sulantha'
+
+PATH = ['/opt/minc-toolkit/bin',
+        '/opt/minc-toolkit/pipeline',
+        '/opt/minc/bin',
+        '/opt/minc-toolkit/bin',
+        '/opt/minc-toolkit/pipeline',
+        '/usr/local/sbin',
+        '/usr/local/bin',
+        '/usr/sbin',
+        '/usr/bin',
+        '/sbin',
+        '/bin']
