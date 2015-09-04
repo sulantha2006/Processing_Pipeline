a92864d245c58c1c66dac2d37f1e7b143dfe9b24
Seqian Wang <seqian@outlook.com>
2015-09-04 12:05:11 -0400

    Rewrote using openpyxl, because the previous one couldn't have more than 256 columns

--- Utils/extractCsvHeaders.py
+++ Utils/extractCsvHeaders.py
@@ -5,37 +5,28 @@ import os
 import csv
 import Config.CsvImportConfig as config
 import openpyxl
+from openpyxl.styles import Font
 
 inputFolder = '/data/data01/wang/adni_csv'
 outputFile = '/home/wang/Downloads/header.xlsx'
 
 
-def extractHeader(csvFile, sqlTable, book, i):
+def extractHeader(csvFile, sqlTable, bookSheet, i):
     # Export the headers from the CSV file and output it to an outputfile
+
+    # Read the header row
     for row in csvFile:
         headerRow = [i.replace('/', '').replace(' ', '').replace('\0', '') for i in row]
         break
-
     headerRow = [sqlTable] + headerRow + ['\n']
 
-    font0 = openpyxl.Font()
-    font0.bold = False
-    style0 = openpyxl.XFStyle()
-    style0.font = font0
-
-    font1 = openpyxl.Font()
-    font1.bold = True
-    style1 = openpyxl.XFStyle()
-    style1.font = font1
-
     j = 0
-    for column in headerRow:
-        if column.lower() in config.uniqueHeaders:
-            style = style1
-        else:
-            style = style0
-        book.write(i, j, column, style)
-        j+= 1
+    for content in headerRow:
+        cell = bookSheet.cell(row = i, column = j)
+        cell.value = content
+        if content.lower() in config.uniqueHeaders:
+            cell.style.font.bold = True
+        j += 1
 
 
 if __name__ == "__main__":
@@ -50,6 +41,6 @@ if __name__ == "__main__":
             csvFile = csv.reader(inputFile)
             extractHeader(csvFile, sqlLocation, bookSheet, i)
 
-        i+= 1
+        i += 1
 
     book.save(outputFile)
