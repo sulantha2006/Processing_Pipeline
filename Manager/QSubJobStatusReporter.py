0cd13334d2b14106b44b2d9dbfa387f71110a621
sulantha <sulantha.s@gmail.com>
2015-09-10 17:56:19 -0400

    Added BeAST QSUB PROCESSING STEPS

--- Manager/QSubJobStatusReporter.py
+++ Manager/QSubJobStatusReporter.py
@@ -11,7 +11,10 @@ class QSubJobStatusReporter:
             nestedJob = job.job
             table = '{0}_{1}_Pipeline'.format(nestedJob.study, nestedJob.modality)
             table_id = nestedJob.table_id
-            setSql = 'UPDATE {0} SET BEAST_MASK = {2} WHERE RECORD_ID = {1}'.format(table, table_id, 1 if status == 'Success' else -1)
+            if status == 'Success':
+                setSql = 'UPDATE {0} SET BEAST_MASK = 1 WHERE RECORD_ID = {1}'.format(table, table_id)
+            elif status == 'Fail':
+                setSql = 'UPDATE {0} SET BEAST_MASK = -1, BEAST_SKIP = 1 WHERE RECORD_ID = {1}'.format(table, table_id)
             self.DBClient.executeNoResult(setSql)
             if status == 'Fail':
                 PipelineLogger.log('manager', 'error','QSUB job Status Failed: - {0} - Processing Table ID : {1} - Modality Table ID : {2}'.format(job.jobType, nestedJob.processing_rid, nestedJob.table_id))
